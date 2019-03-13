1250af77ecb12d368798e370b41a46d8d45adf08
raihan <5421449+ra89fi@users.noreply.github.com>
2019-03-14 01:42:29 +0600

    add Header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = props => {
+  return (
+    <div className="header">
+      <div className="container">
+        <h1 className="header__title">{props.title}</h1>
+        {props.subtitle && <p className="header__subtitle">{props.subtitle}</p>}
+      </div>
+    </div>
+  );
+};
+Header.defaultProps = {
+  title: 'Indecision'
+};
+
+export default Header;
